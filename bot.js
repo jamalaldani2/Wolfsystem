ac64207745ae5d07f5d5d73b510c5e166b770fee
WolfGamer45 <46398256+WolfGamer45@users.noreply.github.com>
2019-01-05 17:08:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -161,6 +161,19 @@ msg.delete();
 
 client.on("message", message => {
  
+            if (message.content.startsWith(prefix + "bco")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//حقوق دايموند كودز
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
+ message.delete();
+};    
+});
+client.on("message", message => {
+ 
             if (message.content.startsWith(prefix + "bc")) {
                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
   let args = message.content.split(" ").slice(1);
@@ -172,7 +185,6 @@ client.on("message", message => {
  message.delete();
 };    
 });
-
 
 
 
