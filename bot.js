97136e11740e1fb457e67de9126825c0468266fc
WolfGamer45 <46398256+WolfGamer45@users.noreply.github.com>
2019-01-05 16:52:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -159,7 +159,19 @@ msg.delete();
 }
 });
 
-
+client.on("message", message => {
+ 
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' ');
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//حقوق دايموند كودز
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
+ message.delete();
+};    
+});
 
 
 
