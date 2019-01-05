837e524a7ea16682b3564c86da377ef7a0a60129
WolfGamer45 <46398256+WolfGamer45@users.noreply.github.com>
2019-01-05 14:54:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,26 +16,7 @@ client.on('message' , message => {
  })
   }  
  });
- client.on('message', fras => {
-var prefix = "$";
-if(omar.content.split(' ')[0] == prefix + 'dc') {  // delete all channels يحذف جميع شاتات
-if (!omar.channel.guild) return;
-if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
-if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
-omar.guild.channels.forEach(m => {
-m.delete();
-});
-}
-if(omar.content.split(' ')[0] == prefix + 'dr') { // delete all roles يحذف جميع رتب
-if (!omar.channel.guild) return;
-if(!omar.guild.member(omar.author).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**You Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
-if(!omar.guild.member(client.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**I Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
-omar.guild.roles.forEach(m => {
-m.delete();
-});//Toxis Codes 
-omar.reply("`تم حذف جميع الرتب بنجاح`")
-}//Toxis Codes
-}); //Toxis Codes
+
 
 
 
