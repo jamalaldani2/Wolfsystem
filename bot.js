d4f25d4ea865d5bd1339106a0b95ac61ca1428cc
jamalaldani2 <45242500+jamalaldani2@users.noreply.github.com>
2019-01-06 19:28:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -160,19 +160,7 @@ msg.delete();
 });
 
 
-client.on("message", message => {
- 
-            if (message.content.startsWith(prefix + "bc1")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' ');
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {//حقوق دايموند كودز
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`);
- message.delete();
-};    
-});
+
 client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name","➥members"));
     });
